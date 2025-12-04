f11e168873c530d778cdbf4154a89061087ae8e2
Abhimanyu <abhimanyukarki0@gmail.com>
2025-12-03 23:05:20 -0600

    thread pool setup start

--- main.cpp
+++ main.cpp
@@ -12,15 +12,26 @@
 
 #pragma comment(lib, "Ws2_32.lib");
 
+constexpr int WORKER_THREADS = 0;   // we can use our custom number of worker threads
+
 enum class OperationType : uint32_t {
 	READ = 1,
 	WRITE = 2
+};
+
+struct PER_SOCKET_CONTEXT {
+	SOCKET socket;
+	std::atomic<bool> closing;
+	// Constructor, allow uninitialized socket and set closing to false (client connection active)
+	PER_SOCKET_CONTEXT(SOCKET s = INVALID_SOCKET) : socket(s), closing(false) {}
 };
 
 void print_wsa_error(const char* msg) {
 	int err = WSAGetLastError();
 	std::cerr << msg << " WSAGetLastError = " << err "\n";
 }
+
+
 
 int main(int argc, char *argv[]) {
 	// Initializing winsock.
@@ -52,6 +63,71 @@ int main(int argc, char *argv[]) {
 	if (setsockopt(listenSocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(resuse)) == SOCKET_ERROR) {
 		print_wsa_error("setsockopt(SO_REUSEADDR failed");
 	}
+
+	sockaddr_in addr{};
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(LISTEN_PORT);
+
+	if (bind(listenSocket, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+		print_wsa_error("bind failed");
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	if (listen(listenSocket, BACKLOG) == SOCKET_ERROR) {
+		print_wsa_error("listen failed");
+		closeSocket("listen failed");
+		WSACleanup();
+		return 1;
+	}
+
+	// if no error occurred, and the listen socket is successfully bound
+	std::cout << "Listening on port " << LISTEN_PORT << std::endl;
+
+	// creating IOCP
+	HANDLE iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, nullptr, 0, 0);
+	if (!iocp) {
+		print_wsa_error("Creating IOCP failed");
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// creating worker thread pool
+	unsigned int cpuCount = std::max(1u, std::thread::hardware_concurrency());
+	unsigned int numWorkers = (WORKER_THREADS > 0) ? WORKER_THREADS : (cpuCount * 2);
+	std::atomic<bool> running{ true };   // atomic bool for thread safety and race condition, also guarantees no reorder of memory operations
+	std::vector<std::thread> workers;
+	workers.reserve(numWorkers);
+
+	std::cout << "Starting " << numWorkers << " worker threads" << endl;
+
+	for (unsigned int i = 0; i < numWorkers; i++) {
+		workers.emplace_back([iocp, &running]()) {
+			while (running.load()) {
+				DWORD bytesTransferred = 0;
+				ULONG_PTR completionKey = 0;
+				LPOVERLAPPED overlapped = nullptr;
+
+				// this is blocking and puts the thread to waiting
+				BOOL ok = GetQueuedCompletionStatus(
+					iocp,
+					&bytesTransferred,
+					&completionKey,
+					&overlapped,
+					INFINITE
+				)
+
+
+			}
+		}
+	}
+
+
+
+
 
 	sockaddr_in service;
 	service.sin_family = AF_NET;
